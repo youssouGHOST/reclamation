78e1e3bd3c97324dbead6e12d3ea0a55bd2c42e1
youssouGHOST <youssou.cisse@uit.ac.ma>
2025-07-21 01:04:20 +0000

    Ajout du workflow de déploiement Amplify Gen2

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,11 +1,14 @@
 import { defineAuth } from '@aws-amplify/backend';
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
 export const auth = defineAuth({
   loginWith: {
     email: true,
   },
+  userAttributes: {
+    email: {
+      required: true,
+      mutable: true,
+    },
+  },
+  groups: ['Admin', 'User', 'Consultant'],
 });
